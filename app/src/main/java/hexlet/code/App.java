b12b7cfe18a6565986fc84941a1d13252c697734
FullBread <FullRe@yandex.ru>
2023-03-23 16:31:22 +0300

    update App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,8 +1,20 @@
 package hexlet.code;
 
+import hexlet.code.games.Even;
+
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Brain Games!");
-        Cli.greetings();
+        Scanner userAnswer = new Scanner(System.in);
+        System.out.println("Please enter the game number and press Enter.\n 1 - Even\n 0 - Exit");
+        int answer = userAnswer.nextInt();
+        switch (answer) {
+            case 0 -> System.out.println("Goodbye :(");
+            case 1 -> {
+                System.out.println("Your choice: " + answer);
+                Even.even();
+            }
+            default -> System.out.println("You entered an invalid value, let's try again.");
+        }
     }
 }
