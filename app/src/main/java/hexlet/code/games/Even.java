ac02d94f2d65b996b42cd1c9601c627849973c5e
FullBread <FullRe@yandex.ru>
2023-03-25 03:10:39 +0300

    update Even

--- app/src/main/java/hexlet/code/games/Even.java
+++ app/src/main/java/hexlet/code/games/Even.java
@@ -1,42 +1,26 @@
 package hexlet.code.games;
 
+import hexlet.code.Engine;
+
 import java.util.Random;
-import java.util.Scanner;
 
 public class Even {
 
     @SuppressWarnings("checkstyle:NeedBraces")
     public static void even() {
-        Scanner answer = new Scanner(System.in);
-        System.out.println("Welcome to the Brain Games!");
-        System.out.print("May I have your name? ");
-        String userName = answer.next();
-        System.out.println("Hello, " + userName + "!");
+        Engine.greetings();
         System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
         Random random = new Random();
-        var count = 0;
-        var correctAnswer = "";
-        while (count < 3) {
-            int randomNumber = random.nextInt(100);
-            if (randomNumber % 2 == 0) {
-                correctAnswer = "yes";
+        String[] evenArray = new String[3];
+        String[] answerEven = new String[3];
+        for (var i = 0; i < 3; i++) {
+            evenArray[i] = String.valueOf(random.nextInt(100));
+            if (Integer.parseInt(evenArray[i]) % 2 == 0) {
+                answerEven[i] = "yes";
             } else {
-                correctAnswer = "no";
-            }
-            System.out.println("Question: " + randomNumber);
-            String userAnswer = answer.next();
-            System.out.println("Your answer: " + userAnswer);
-            if (correctAnswer.equals(userAnswer)) {
-                count += 1;
-                System.out.println("Correct!");
-                if (count == 3) {
-                    System.out.println("Congratulations, " + userName + "!");
-                }
-            } else {
-                System.out.printf("'%s' is wrong answer ;(. Correct answer was '%s'.", userAnswer, correctAnswer);
-                System.out.println("\n Let's try again, " + userName + "!");
-                break;
+                answerEven[i] = "no";
             }
         }
+        Engine.correctOrNot(evenArray, answerEven);
     }
 }
