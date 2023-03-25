7a24cb8f53ef877cc2a1964e3cc8f8eb6541e9a7
FullBread <FullRe@yandex.ru>
2023-03-25 14:37:53 +0300

    add GCD

--- app/src/main/java/hexlet/code/games/GCD.java
+++ app/src/main/java/hexlet/code/games/GCD.java
@@ -1,2 +1,28 @@
-package hexlet.code.games;public class GCD {
+package hexlet.code.games;
+
+import hexlet.code.Engine;
+
+import java.util.Random;
+
+public class GCD {
+
+    public static void gcd() {
+        Engine.greetings();
+        System.out.println("Find the greatest common divisor of given numbers.");
+        Random random = new Random();
+        String[] gcdArray = new String[3];
+        String[] answerArray = new String[3];
+        for (var i = 0; i < 3; i++) {
+            var numberOne = random.nextInt(40, 260);
+            var numberTwo = random.nextInt(40, 260);
+            gcdArray[i] = numberOne + " " + numberTwo;
+            while (numberTwo != 0) {
+                int temp = numberTwo;
+                numberTwo = numberOne % numberTwo;
+                numberOne = temp;
+            }
+            answerArray[i] = String.valueOf(numberOne);
+        }
+        Engine.correctOrNot(gcdArray, answerArray);
+    }
 }
