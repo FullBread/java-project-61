b89cdfbbef5db651951cbbcbe0d8393c37915bcd
FullBread <FullRe@yandex.ru>
2023-03-25 14:36:58 +0300

    update app

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -2,6 +2,7 @@ package hexlet.code;
 
 import hexlet.code.games.Calc;
 import hexlet.code.games.Even;
+import hexlet.code.games.GCD;
 
 import java.util.Scanner;
 public class App {
@@ -11,6 +12,7 @@ public class App {
                 Please enter the game number and press Enter.
                 1 - Even
                 2 - Calc
+                3 - GCD
                 0 - Exit""");
         System.out.print("Your choice: ");
         int answer = userAnswer.nextInt();
@@ -18,6 +20,7 @@ public class App {
             case 0 -> System.out.println("Goodbye :(");
             case 1 -> Even.even();
             case 2 -> Calc.calc();
+            case 3 -> GCD.gcd();
             default -> System.out.println("You entered an invalid value, let's try again.");
         }
     }
