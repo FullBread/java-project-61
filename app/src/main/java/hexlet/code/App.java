c5ad3856efa282db7bd48732829144e1502f5619
FullBread <FullRe@yandex.ru>
2023-03-27 20:12:47 +0300

    update App

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -3,6 +3,7 @@ package hexlet.code;
 import hexlet.code.games.Calc;
 import hexlet.code.games.Even;
 import hexlet.code.games.GCD;
+import hexlet.code.games.Prime;
 import hexlet.code.games.Progression;
 
 import java.util.Scanner;
@@ -12,10 +13,11 @@ public class App {
         System.out.println("""
                 Please enter the game number and press Enter.
                 1 - Greet
-                1 - Even
-                2 - Calc
-                3 - GCD
-                4 - Progression
+                2 - Even
+                3 - Calc
+                4 - GCD
+                5 - Progression
+                6 - Prime
                 0 - Exit""");
         System.out.print("Your choice: ");
         int answer = userAnswer.nextInt();
@@ -26,6 +28,7 @@ public class App {
             case 3 -> Calc.calc();
             case 4 -> GCD.gcd();
             case 5 -> Progression.progression();
+            case 6 -> Prime.prime();
             default -> System.out.println("You entered an invalid value, let's try again.");
         }
     }
