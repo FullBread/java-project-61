6c97070cf057917213c5b0d63322ca45a8466f1e
FullBread <FullRe@yandex.ru>
2023-03-27 20:13:59 +0300

    add Prime

--- app/src/main/java/hexlet/code/games/Prime.java
+++ app/src/main/java/hexlet/code/games/Prime.java
@@ -1,2 +1,26 @@
-package hexlet.code.games;public class Prime {
+package hexlet.code.games;
+
+import hexlet.code.Engine;
+
+import org.apache.commons.math3.primes.Primes;
+import java.util.Random;
+
+public class Prime {
+
+    public static void prime() {
+        Engine.greetings();
+        System.out.println("Answer 'yes' if given number is prime. Otherwise answer 'no'.");
+        Random random = new Random();
+        String[] primeArray = new String[3];
+        String[] answerArray = new String[3];
+        for (var i = 0; i < 3; i++) {
+            primeArray[i] = String.valueOf(random.nextInt(120));
+            if (Primes.isPrime(Integer.parseInt(primeArray[i]))) {
+                answerArray[i] = "yes";
+            } else {
+                answerArray[i] = "no";
+            }
+        }
+        Engine.correctOrNot(primeArray, answerArray);
+    }
 }
