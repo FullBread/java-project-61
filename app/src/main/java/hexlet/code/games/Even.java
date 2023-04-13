80bcd2a09ad351186a2e4c306c9697c82d87d7fa
FullBread <FullRe@yandex.ru>
2023-04-13 12:50:52 +0300

    fix Even

--- app/src/main/java/hexlet/code/games/Even.java
+++ app/src/main/java/hexlet/code/games/Even.java
@@ -8,16 +8,17 @@ public class Even {
     private static final int COUNT_OF_QUESTIONS = 3;
     private static final int UPPER_BOUND = 100;
 
+    private static boolean isEven(int number) {
+        return number % 2 == 0;
+    }
 
-    @SuppressWarnings("checkstyle:NeedBraces")
     public static void even() {
         String gameRules = "Answer 'yes' if the number is even, otherwise answer 'no'.";
         Random random = new Random();
         String[][] evenArray = new String[COUNT_OF_QUESTIONS][2];
         for (var i = 0; i < COUNT_OF_QUESTIONS; i++) {
             evenArray[i][0] = String.valueOf(random.nextInt(UPPER_BOUND));
-            boolean evenOrNot = Integer.parseInt(evenArray[i][0]) % 2 == 0;
-            evenArray[i][1] = evenOrNot ? "yes" : "no";
+            evenArray[i][1] = isEven(Integer.parseInt(evenArray[i][0])) ? "yes" : "no";
         }
         Engine.play(gameRules, evenArray);
     }
